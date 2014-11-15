9e5807eaf38c65aa6d588d9fb7ea786cb6acedcb
Dale <damarkowitz@gmail.com>
2014-11-15 18:06:28 -0500

    plotting looks better

--- plotly.py
+++ plotly.py
@@ -5,8 +5,11 @@ from plotly.graph_objs import *
 import numpy as np
 import random
 import sys, serial
-SERIAL = False
-if SERIAL:  ser = serial.Serial('/tmp/tty.LightBlue-Bean', 9600)
+SERIAL = True
+#SERIAL_PORT = '/tmp/tty.LightBlue-Bean'
+SERIAL_PORT = '/dev/tty.usbmodem1411'
+if SERIAL:
+    ser = serial.Serial(SERIAL_PORT, 57600)
 
 random.seed()
 py.sign_in("dalequark", "zgfdgx1clm")
@@ -24,21 +27,20 @@ stream_ids = tls.get_credentials_file()['stream_ids']
 # Make instance of stream id object
 stream1 = Stream(
     token=stream_ids[0],  # (!) link stream id to 'token' key
-    maxpoints=120      # (!) keep a max of 120 pts on screen
+    maxpoints=300      # (!) keep a max of 120 pts on screen
 )
 stream2 = Stream(
     token=stream_ids[1],  # (!) link stream id to 'token' key
     maxpoints=120      # (!) keep a max of 120 pts on screen
 )
 
-
-
 # Initialize trace of streaming plot by embedding the unique stream_id
 trace1 = Scatter(
     x=[],
     y=[],
     mode='lines+markers',
-    stream=stream1         # (!) embed stream id, 1 per trace
+    stream=stream1         # (!) embed stream id, 1 per trace,
+
 )
 
 trace2 = Scatter(
@@ -46,6 +48,7 @@ trace2 = Scatter(
     y=[],
     mode='lines+markers',
     stream=stream2         # (!) embed stream id, 1 per trace
+
 )
 
 data = Data([trace1, trace2])
@@ -66,16 +69,30 @@ s2 = py.Stream(stream_ids[1])
 s1.open()
 s2.open()
 
+SECOND_CHANNEL = False
+i = 0
 while True:
     x=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     if SERIAL:
         line = ser.readline()
-    else:
-        line = random.randint(0,10)
+        line = line.split()
+        if(len(line) < 2):  continue
+        if(line[0].isdigit()):
+            data1 =  (float(line[0]) * 5 / 1023 )
+            data2 =  (float(line[1]) * 5 / 1023 )
+            print data1
+            if(i%5 == 0):   s1.write( dict( x=x, y= data1 ))
+            i += 1
 
-    y=float(line)
-    z=float(random.randint(0,10))
-    s1.write(dict(x=x,y=y))
-    s2.write(dict(x=x,y=z))
-s1.close()
-s2.close()
+        if(SECOND_CHANNEL):
+            if(line[1].isdigit()):
+                s2.write( dict(x=x, y=data2) )
+    else:
+        int1 =random.randint(0,10)
+        int2 =random.randint(0,10)
+        s1.write(dict(x=x,y=int1))
+        s2.write(dict(x=x,y=int2))
+
+if SERIAL:
+    s1.close()
+    s2.close()
